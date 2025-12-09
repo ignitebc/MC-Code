5900b2faf007047c7f2d003346eed945d0addc1d
정재학 <ignitebc@udmtek.com>
2025-12-09 16:25:58 +0900

    - 사냥꾼, 대장장이 스킬조정 및 추가 - 비트코인 획득시 전체 메일 발송 - 비트코인 확률 조정 - 패치노트 수정

--- JobsPlusRemastered-1.21.9/common/src/main/java/com/daqem/jobsplus/integration/arc/reward/type/JobsPlusRewardType.java
+++ JobsPlusRemastered-1.21.9/common/src/main/java/com/daqem/jobsplus/integration/arc/reward/type/JobsPlusRewardType.java
@@ -4,6 +4,7 @@ import com.daqem.arc.api.reward.IReward;
 import com.daqem.arc.api.reward.type.IRewardType;
 import com.daqem.arc.api.reward.type.RewardType;
 import com.daqem.jobsplus.JobsPlus;
+import com.daqem.jobsplus.integration.arc.reward.rewards.job.JobBitcoinReward;
 import com.daqem.jobsplus.integration.arc.reward.rewards.job.JobCoinReward;
 import com.daqem.jobsplus.integration.arc.reward.rewards.job.JobExpReward;
 import com.daqem.jobsplus.integration.arc.reward.rewards.job.JobExpMultiplierReward;
@@ -14,7 +15,9 @@ public interface JobsPlusRewardType<T extends IReward> extends RewardType<T>
     IRewardType<JobExpReward> JOB_EXP = RewardType.register(JobsPlus.getId("job_exp"), new JobExpReward.Serializer());
     IRewardType<JobExpMultiplierReward> JOB_EXP_MULTIPLIER = RewardType.register(JobsPlus.getId("job_exp_multiplier"), new JobExpMultiplierReward.Serializer());
     IRewardType<JobCoinReward> JOB_COIN = RewardType.register(JobsPlus.getId("job_coin"), new JobCoinReward.Serializer());
-
+    
+    //bitcoin reword 추가
+    IRewardType<JobBitcoinReward> BITCOIN_REWARD = RewardType.register(JobsPlus.getId("bitcoin_reward"), new JobBitcoinReward.Serializer());
     static void init()
     {
     }
