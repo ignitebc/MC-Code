ee8ef0cc3cf6b3c9318e57455fe86d6bb5be8389
JaeHak Jeong <ignitebc@gmail.com>
2026-07-26 12:41:55 +0900

    design: 직업 경험치 안내 화면 스크롤 개편
    - 페이지 버튼과 조건 컴포넌트를 제거하고 연속 스크롤 구조로 변경
    - 경험치 항목별 제목과 상세 설명을 읽기 쉽게 배치

---
+++ JobsPlusRemastered-1.21.10/common/src/main/java/com/daqem/jobsplus/client/gui/jobs/components/ExperienceScrollContentComponent.java
@@ -0,0 +1,71 @@
+package com.daqem.jobsplus.client.gui.jobs.components;
+
+import com.daqem.arc.api.action.IAction;
+import com.daqem.jobsplus.JobsPlus;
+import com.daqem.jobsplus.client.gui.jobs.JobsScreenState;
+import com.daqem.jobsplus.integration.arc.reward.rewards.job.JobExpReward;
+import com.daqem.uilib.gui.component.EmptyComponent;
+import com.daqem.uilib.gui.component.text.multiline.MultiLineTextComponent;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ExperienceScrollContentComponent extends EmptyComponent
+{
+
+    private static final int SECTION_GAP = 9;
+    private static final int GUIDE_CONTENT_GAP = 2;
+    private static final int HEADER_COLOR = 0xFFB22222;
+    private static final int CONTENT_COLOR = 0xFF1E1410;
+
+    public ExperienceScrollContentComponent(JobsScreenState state, int width)
+    {
+        super(0, 0, width, 0);
+
+        List<IAction> actions = state.getSelectedJob().getJobInstance().getActions().stream()
+                .filter(action -> action.getRewards().stream().anyMatch(JobExpReward.class::isInstance))
+                .sorted(Comparator.comparingInt(this::getMaximumExperience)
+                        .thenComparing(action -> action.getLocation().getPath()))
+                .toList();
+
+        MultiLineTextComponent guideHeaderComponent = new MultiLineTextComponent(
+                0,
+                0,
+                getWidth(),
+                JobsPlus.translatable("gui.jobs.experience.guide.header"),
+                HEADER_COLOR);
+        MultiLineTextComponent guideContentComponent = new MultiLineTextComponent(
+                0,
+                guideHeaderComponent.getHeight() + GUIDE_CONTENT_GAP,
+                getWidth(),
+                JobsPlus.translatable("gui.jobs.experience.guide.description"),
+                CONTENT_COLOR);
+        this.addComponent(guideHeaderComponent);
+        this.addComponent(guideContentComponent);
+
+        int yOffset = guideHeaderComponent.getHeight()
+                + GUIDE_CONTENT_GAP
+                + guideContentComponent.getHeight()
+                + SECTION_GAP;
+        String jobPath = state.getSelectedJob().getJobInstance().getLocation().getPath();
+        for (IAction action : actions)
+        {
+            ActionItemComponent actionItemComponent = new ActionItemComponent(action, jobPath, getWidth());
+            actionItemComponent.setY(yOffset);
+            this.addComponent(actionItemComponent);
+            yOffset += actionItemComponent.getHeight() + SECTION_GAP;
+        }
+
+        this.setHeight(Math.max(0, yOffset - SECTION_GAP));
+    }
+
+    private int getMaximumExperience(IAction action)
+    {
+        return action.getRewards().stream()
+                .filter(JobExpReward.class::isInstance)
+                .map(JobExpReward.class::cast)
+                .mapToInt(JobExpReward::getMax)
+                .findFirst()
+                .orElse(0);
+    }
+}
--- JobsPlusRemastered-1.21.10/common/src/main/java/com/daqem/jobsplus/client/networking/ClientboundOpenJobsScreenPacketHandler.java
+++ JobsPlusRemastered-1.21.10/common/src/main/java/com/daqem/jobsplus/client/networking/ClientboundOpenJobsScreenPacketHandler.java
@@ -1,6 +1,5 @@
 package com.daqem.jobsplus.client.networking;
 
-import com.daqem.arc.api.action.IAction;
 import com.daqem.jobsplus.client.gui.jobs.JobsScreen;
 import com.daqem.jobsplus.client.gui.jobs.JobsScreenState;
 import com.daqem.jobsplus.client.gui.jobs.tab.RightTab;
@@ -36,8 +35,6 @@ public class ClientboundOpenJobsScreenPacketHandler {
             Job keepJob = findSameJobOrFirst(jobs, oldState.getSelectedJob());
             @Nullable
             ShopOffer keepOffer = oldState.getSelectedShopOffer();
-            @Nullable
-            IAction keepAction = oldState.getActiveAction();
             String keepStockId = oldState.getSelectedStockId();
             String keepHoldingStockId = oldState.getSelectedHoldingStockId();
             var keepStockPanelMode = oldState.getStockPanelMode();
@@ -45,7 +42,6 @@ public class ClientboundOpenJobsScreenPacketHandler {
             JobsScreenState newState = new JobsScreenState(
                     jobs, coins, maxJobs, keepJob, keepTab, packet.getStockAccount());
             newState.setSelectedShopOffer(keepOffer);
-            newState.setActiveAction(keepAction);
             newState.setSelectedStockId(keepStockId);
             newState.setSelectedHoldingStockId(keepHoldingStockId);
             newState.setStockPanelMode(keepStockPanelMode);
