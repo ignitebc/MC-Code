40854145fd14bddd8d78b3e0fffd680203202d17
정재학 <ignitebc@udmtek.com>
2026-08-10 11:08:03 +0900

    fix: 화살비 추가 화살에 독 효과가 복사되지 않던 문제 수정
    - 추가 화살 생성 시 화염과 함께 독 지속시간·강화 수치도 복사
    - 맹독 불화살과 화살비 동시 사용 시 추가 화살에도 독 적용

--- ArcLib-26.2/common/src/main/java/com/daqem/arc/data/reward/entity/MultipleArrowsReward.java
+++ ArcLib-26.2/common/src/main/java/com/daqem/arc/data/reward/entity/MultipleArrowsReward.java
@@ -110,6 +110,10 @@ public class MultipleArrowsReward extends AbstractReward {
             projectile.setRemainingFireTicks(shotArrow.getRemainingFireTicks());
             ((IArcAbstractArrow) projectile).arc$setFireDurationTicks(
                     ((IArcAbstractArrow) shotArrow).arc$getFireDurationTicks());
+            ((IArcAbstractArrow) projectile).arc$setPoisonDurationTicks(
+                    ((IArcAbstractArrow) shotArrow).arc$getPoisonDurationTicks());
+            ((IArcAbstractArrow) projectile).arc$setPoisonAmplifier(
+                    ((IArcAbstractArrow) shotArrow).arc$getPoisonAmplifier());
             Vec3 vec3 = player.getUpVector(1.0f);
             Quaternionf quaternionf = new Quaternionf().setAngleAxis(pitch * ((float)Math.PI / 180), vec3.x, vec3.y, vec3.z);
             Vec3 vec32 = player.getViewVector(1.0f);
