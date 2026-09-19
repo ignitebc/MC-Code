352a67f773584d051711cd1bc516da0838320775
JaeHak Jeong <ignitebc@gmail.com>
2026-09-19 09:32:27 +0900

    design: J 메뉴 탭 이름과 순서 조정
    - 경험치획득 탭을 직업정보로 이름을 바꾼다
    - 직업정보, 주식, 상점, 총기 도감, 게임안내 순으로 재배치한다

--- JobsPlusRemastered-26.2/common/src/main/java/com/daqem/jobsplus/client/gui/jobs/tab/RightTab.java
+++ JobsPlusRemastered-26.2/common/src/main/java/com/daqem/jobsplus/client/gui/jobs/tab/RightTab.java
@@ -3,11 +3,11 @@ package com.daqem.jobsplus.client.gui.jobs.tab;
 import net.minecraft.network.chat.Component;
 
 public enum RightTab implements ITab {
-    EXPERIENCE(Component.literal("경험치획득")),
-    RECIPES(Component.literal("게임안내")),
+    EXPERIENCE(Component.literal("직업정보")),
     UP_AND_DOWN(Component.literal("주식")),
     SHOP(Component.literal("상점")),
-    GUN_GUIDE(Component.literal("총기 도감"));
+    GUN_GUIDE(Component.literal("총기 도감")),
+    RECIPES(Component.literal("게임안내"));
 
     private final Component name;
 
