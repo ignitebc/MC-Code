c13aad0b40f89c305991d6fd79188028867cca81
JaeHak Jeong <ignitebc@gmail.com>
2026-09-20 18:19:39 +0900

    feat: 몬스터도 탄창을 소모하고 다 쓰면 재장전
    - 몬스터가 쏠 때마다 탄창이 줄고, 비면 재장전 시간만큼 사격을 멈춘 뒤 다시 발사
    - 탄약 아이템은 확인하지 않으므로 재장전 횟수에 제한이 없고 탄창 크기는 부착물 없는 기본값

--- TACZ-Refabricated-26.2/src/main/java/com/tacz/guns/entity/shooter/LivingEntityAmmoCheck.java
+++ TACZ-Refabricated-26.2/src/main/java/com/tacz/guns/entity/shooter/LivingEntityAmmoCheck.java
@@ -11,6 +11,10 @@ public class LivingEntityAmmoCheck {
         this.shooter = shooter;
     }
 
+    /**
+     * 재장전할 때 탄약 아이템이 있는지 확인하고 소모할지.
+     * 몬스터는 탄약 아이템을 갖지 않으므로 확인하지 않는다. 그래서 재장전하면 탄창이 그냥 가득 찬다.
+     */
     public boolean needCheckAmmo() {
         if (shooter instanceof Player player) {
             return !player.isCreative();
@@ -18,10 +22,15 @@ public class LivingEntityAmmoCheck {
         return !MonsterGunController.isMonster(shooter);
     }
 
+    /**
+     * 쏠 때 탄창의 탄을 줄일지.
+     * 몬스터도 줄인다. 탄창을 다 쓰면 {@link MonsterGunController}가 재장전을 걸고 장전 시간만큼 사격을 멈춘다.
+     * 재장전 횟수에는 제한이 없다.
+     */
     public boolean consumesAmmoOrNot() {
         if (shooter instanceof Player player) {
             return !player.isCreative() || GunConfig.CREATIVE_PLAYER_CONSUME_AMMO.get();
         }
-        return !MonsterGunController.isMonster(shooter);
+        return true;
     }
 }
